74f0c556891da0faa4cf4c9992992823be43828f
smartvain <zlc.blue15210@gmail.com>
2022-11-20 21:28:01 +0900

    add: stats

--- src/client/client.ts
+++ src/client/client.ts
@@ -1,5 +1,6 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+import Stats from 'three/examples/jsm/libs/stats.module'
 
 const scene = new THREE.Scene()
 
@@ -15,7 +16,8 @@ const renderer = new THREE.WebGLRenderer()
 renderer.setSize(window.innerWidth, window.innerHeight)
 document.body.appendChild(renderer.domElement)
 
-new OrbitControls(camera, renderer.domElement)
+const controls = new OrbitControls(camera, renderer.domElement)
+// controls.addEventListener('change', render)
 
 const geometry = new THREE.BoxGeometry()
 const material = new THREE.MeshBasicMaterial({
@@ -34,13 +36,19 @@ function onWindowResize() {
     render()
 }
 
+const stats = Stats()
+document.body.appendChild(stats.dom)
+
 function animate() {
     requestAnimationFrame(animate)
 
+    // stats.begin()
     cube.rotation.x += 0.01
     cube.rotation.y += 0.01
+    // stats.end()
 
     render()
+    stats.update()
 }
 
 function render() {
@@ -48,3 +56,4 @@ function render() {
 }
 
 animate()
+// render()
